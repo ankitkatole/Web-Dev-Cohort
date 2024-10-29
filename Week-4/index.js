6acead5a5f804aa37c96144c14b7f0517dc065d0
ankitkatole <katoleankit06@gmail.com>
2024-10-29 21:02:24 +0530

    Week-4 P1

---
+++ Week-4/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'a.txt');
+console.log(__dirname)
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
