e348c0c7860877e95f93cb440097ef7155b489ca
ankitkatole <katoleankit06@gmail.com>
2024-10-31 21:56:09 +0530

    Week 5

--- Week-4/index.js
+++ Week-4/index.js
@@ -1,13 +1,37 @@
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
 
-const filePath = path.join(__dirname, 'a.txt');
-console.log(__dirname)
+const app = express();
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+app.use(function(req, res, next) {
+  console.log("request received");
+  next();
+})
+
+app.get("/sum",(req,res)=>{
+  console.log(parseInt(req.query.a)+parseInt(req.query.b))
+    res.json({
+      ans: parseInt(req.query.a)+parseInt(req.query.b)
+    })
+})
+app.get("/multiply",(req,res)=>{
+  console.log(req.query.a*req.query.b)
+    res.json({
+      ans: req.query.a*req.query.b
+    })
+})
+app.get("/divide",(req,res)=>{
+  console.log(req.query.a/req.query.b)
+    res.json({
+      ans: req.query.a/req.query.b
+    })
+})
+app.get("/substract",(req,res)=>{
+  console.log(req.query.a-req.query.b)
+    res.json({
+      ans: req.query.a-req.query.b
+    })
+})
+
+app.listen(3000,()=>{
+  console.log("Started Listening")
 });
