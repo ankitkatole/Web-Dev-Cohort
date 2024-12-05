6f27c9c3f76c19b2709f44180225fa189e543227
ankitkatole <katoleankit06@gmail.com>
2024-12-05 11:57:56 +0530

    initialized project and added skeleton route

---
+++ Week-8-csApp/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.post("/user/signup",(req,res)=>{
+    
+})
+
+app.post("/user/signin",(req,res)=>{
+
+})
+
+app.get("/user/purchases",(req,res)=>{
+
+})
+
+app.post("/course/purchase", (req,res)=>{
+
+})
+
+app.get("/courses",(req,res)=>{
+
+})
+
+app.listen(3000);
