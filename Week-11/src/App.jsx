5f56c7b40bbe41c8a1cb758043a7aded66f4e45e
ankitkatole <katoleankit06@gmail.com>
2024-12-13 11:09:55 +0530

    Recoil in react

--- Week-11/src/App.jsx
+++ Week-11/src/App.jsx
@@ -1,58 +1,101 @@
-import { useState, createContext, useContext } from "react"
-import './App.css'
+import React, { createContext, useContext, useState } from 'react';
+import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from 'recoil';
 
-const BulbContext = createContext();
+const count = atom({
+  key: 'countState', // unique ID (with respect to other atoms/selectors)
+  default: 0, // default value (aka initial value)
+});
 
-function BulbProvider({ children }) {
-  const [bulbOn, setBulbOn] = useState(true);
+function Parent() {
   return (
-    <BulbContext.Provider value={{ bulbOn, setBulbOn }}>
-      {children}
-    </BulbContext.Provider>
-  )
+    <RecoilRoot>
+      <Incrase />
+      <Decrease />
+      <Value />
+    </RecoilRoot>
+  );
 }
 
-
-function App() {
-  return (
-    <>
-      <BulbProvider>
-        <LightBulb />
-      </BulbProvider>
-    </>
-  )
+function Decrease() {
+  const setCount = useSetRecoilState(count);
+  return <button onClick={() => setCount(count => count - 1)}>Decrease</button>;
 }
 
+function Incrase() {
+  const setCount = useSetRecoilState(count);
+  return <button onClick={() => setCount(count => count + 1)}>Increase</button>;
+}
 
+function Value() {
+  const countValue = useRecoilValue(count);
+  return <p>Count: {countValue}</p>;
+}
 
-const LightBulb = () => {
+// App Component
+const App = () => {
   return <div>
-    <BulbState />
-    <ToggleBulbState />
+    <Parent />
   </div>
-}
-
-
-
-const BulbState = () => {
-  const { bulbOn } = useContext(BulbContext)
-  return <div>
-    {bulbOn ? "Bulb On" : "Bulb Off"}
-  </div>
-}
-
-
-
-const ToggleBulbState = () => {
-  const { bulbOn, setBulbOn } = useContext(BulbContext)
-  function toggle() {
-    setBulbOn(!bulbOn)
-  }
-  return <div>
-    <button onClick={toggle}>Toggle Bulb</button>
-  </div>
-}
-
-
+};
 
 export default App;
+
+
+// import { useState, createContext, useContext } from "react"
+// import './App.css'
+
+// const BulbContext = createContext();
+
+// function BulbProvider({ children }) {
+//   const [bulbOn, setBulbOn] = useState(true);
+//   return (
+//     <BulbContext.Provider value={{ bulbOn, setBulbOn }}>
+//       {children}
+//     </BulbContext.Provider>
+//   )
+// }
+
+
+// function App() {
+//   return (
+//     <>
+//       <BulbProvider>
+//         <LightBulb />
+//       </BulbProvider>
+//     </>
+//   )
+// }
+
+
+
+// const LightBulb = () => {
+//   return <div>
+//     <BulbState />
+//     <ToggleBulbState />
+//   </div>
+// }
+
+
+
+// const BulbState = () => {
+//   const { bulbOn } = useContext(BulbContext)
+//   return <div>
+//     {bulbOn ? "Bulb On" : "Bulb Off"}
+//   </div>
+// }
+
+
+
+// const ToggleBulbState = () => {
+//   const { bulbOn, setBulbOn } = useContext(BulbContext)
+//   function toggle() {
+//     setBulbOn(!bulbOn)
+//   }
+//   return <div>
+//     <button onClick={toggle}>Toggle Bulb</button>
+//   </div>
+// }
+
+
+
+// export default App;
