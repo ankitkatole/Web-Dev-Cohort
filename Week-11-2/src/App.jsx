e08dc10cff2ddae993e4d87491471855791f38e5
ankitkatole <katoleankit06@gmail.com>
2024-12-16 18:56:54 +0530

    Recoil Atoms and Selectors

--- Week-11-2/src/App.jsx
+++ Week-11-2/src/App.jsx
@@ -1,36 +1,113 @@
 import { useState } from 'react'
 import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtom, evenSelector } from './store/atoms/counter'
 
 function App() {
- return (
+  return (
     <>
-      <Counter />
+      <RecoilRoot>
+        <Counter />
+      <br />
+      <Buttons />
+      <Count />
+      <IsEven />
+      </RecoilRoot>
     </>
   )
 }
 
-const Counter = ()=>{
-  const [count,setCount] = useState(0);
-  return <>
-    {count}<br />
-    <Increase setCount = {setCount}/><br />
-    <Decrease setCount = {setCount}/>
-  </>
+function Buttons() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  function increase() {
+    setCount(c => c + 1);
+  }
+  function decrease() {
+    setCount(c => c - 1);
+  }
+
+  return <div>
+    <button onClick={increase}>Increase</button>
+    <button onClick={decrease}>Decrease</button>
+  </div>
 }
 
-const Increase = ({setCount})=>{
+function Count() {
+  const count2 = useRecoilValue(counterAtom);
   return <div>
-    <button onClick={()=>{
-      setCount((prev)=>prev+1)
+    {count2}<br />
+  </div>
+}
+
+function IsEven() {
+  const even = useRecoilValue(evenSelector);
+
+  return <div>
+    {even ? "Even" : "Odd"}
+  </div>
+}
+
+function Counter() {
+
+  return <div>
+    <h1>Counter</h1>
+    <CurrentCount /><br />
+    <Increase /><br />
+    <Decrease />
+  </div>
+}
+
+const CurrentCount = () => {
+  const count = useRecoilValue(counterAtom);
+  return <div>
+    {count}<br />
+  </div>
+}
+
+const Increase = () => {
+  const setCount = useSetRecoilState(counterAtom);
+  return <div>
+    <button onClick={() => {
+      setCount((prev) => prev + 1)
     }}>Increase</button>
   </div>
 }
-const Decrease = ({setCount})=>{
+
+const Decrease = () => {
+  const setCount = useSetRecoilState(counterAtom);
   return <div>
-    <button onClick={()=>{
-      setCount((prev)=>prev-1)
+    <button onClick={() => {
+      setCount((prev) => prev - 1)
     }}>Decrease</button>
   </div>
 }
 
 export default App
+
+
+//Using UseState Hook
+
+// const Counter = ()=>{
+//   const [count,setCount] = useState(0);
+//   return <>
+//     {count}<br />
+//     <Increase setCount = {setCount}/><br />
+//     <Decrease setCount = {setCount}/>
+//   </>
+// }
+
+// const Increase = ({setCount})=>{
+//   return <div>
+//     <button onClick={()=>{
+//       setCount((prev)=>prev+1)
+//     }}>Increase</button>
+//   </div>
+// }
+// const Decrease = ({setCount})=>{
+//   return <div>
+//     <button onClick={()=>{
+//       setCount((prev)=>prev-1)
+//     }}>Decrease</button>
+//   </div>
+// }
