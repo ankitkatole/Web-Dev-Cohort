8bbef87bc09fb423cca665a32487bde4a2540903
ankitkatole <katoleankit06@gmail.com>
2025-03-03 22:02:05 +0530

    Commit

---
+++ Week-14/src/index.ts
@@ -0,0 +1,50 @@
+// // let x: number = 1 //type inferencing
+// // x="harkirat"; //error
+// // console.log(x);
+
+// function greet(name: string){
+//     console.log("Hello",name);
+// }
+
+// function sum(a: number, b: number){
+//     return a+b;
+// }
+
+// greet("Jitesh");
+// console.log(sum(1,2));
+
+// function isLegal(age: number): boolean{
+//     return age>=18;
+// }
+
+// console.log(isLegal(19));
+// console.log(isLegal(17));
+
+// function delayedCall(anotherFunction: ()=> void){
+    // setTimeout(anotherFunction, 5000);
+// }
+// 
+// function log(){
+    // console.log("Hi there")
+// }
+// 
+// delayedCall(log);
+
+// let greet = ()=>{
+    // console.log("Hello");
+// }
+// 
+// greet();
+
+// function greet(user:{
+//     name: string,
+//     age: number
+// }){
+//     console.log("Hello",user.name);
+//     console.log("You are",user.age,"years old");
+// }
+
+// greet({name: "Gian", age: 20});
+
+// console.log(console.log("Hello")); //undefined
+
